0736eef152cb0bf3877e084ab0397b1a8fcd6f57
Fabio <faironsp@gmail.com>
2020-06-02 21:25:52 -0300

    Alterando o padrão da nomenclatura do controller para o plural

--- Api.Application/Controllers/UsersController.cs
+++ Api.Application/Controllers/UsersController.cs
@@ -9,12 +9,12 @@ namespace Api.Application.Controllers
 {
     [ApiController]
     [Route("[controller]")]
-    public class UserController : ControllerBase
+    public class UsersController : ControllerBase
     {
         private BaseService<User> service = new BaseService<User>();
-        private readonly ILogger<UserController> _logger;
+        private readonly ILogger<UsersController> _logger;
 
-        public UserController(ILogger<UserController> logger)
+        public UsersController(ILogger<UsersController> logger)
         {
             _logger = logger;
         }
