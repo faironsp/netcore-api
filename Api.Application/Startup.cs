1ae173139b29063c34c96252d039c834eb451ff1
Fabio <faironsp@gmail.com>
2020-06-05 14:58:09 -0300

    Conn

--- Api.Application/Startup.cs
+++ Api.Application/Startup.cs
@@ -23,7 +23,7 @@ namespace Api.Application
             {
                 options.AddPolicy(
                   "CorsPolicy",
-                  builder => builder.AllowAnyOrigin()
+                  builder => builder.WithOrigins("http://localhost:4200")//builder => builder.AllowAnyOrigin()
                   .AllowAnyMethod()
                   .AllowAnyHeader()
                   .AllowCredentials());
