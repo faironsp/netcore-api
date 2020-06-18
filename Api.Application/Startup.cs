33d9b7fead9e7302026e7c560dad84b850f72371
Fabio <faironsp@gmail.com>
2020-06-18 19:16:33 -0300

    Cors

--- Api.Application/Startup.cs
+++ Api.Application/Startup.cs
@@ -23,7 +23,12 @@ namespace Api.Application
             {
                 options.AddPolicy(
                   "CorsPolicy",
-                  builder => builder.WithOrigins("http://localhost:4200", "http://app.aprovatto.com.br", "https://app.aprovatto.com.br")
+                  builder => builder.WithOrigins(
+                      "http://localhost:4200",
+                      "http://app.aprovatto.com.br",
+                      "https://app.aprovatto.com.br",
+                      "http://sys.aprovatto.com.br",
+                      "https://sys.aprovatto.com.br")
                   .AllowAnyMethod()
                   .AllowAnyHeader()
                   .AllowCredentials());
